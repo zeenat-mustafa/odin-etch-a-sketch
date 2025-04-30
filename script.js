21096b7d1145fb9cf8a8716df859c83e78bd4677
Zeenat Mustafa <chzeenatmustafa2310@gmail.com>
2025-04-30 22:26:13 +0500

    Add a button to change the no. of squares per side
    - Add the grid creating method into a createGrid function
    - Create a new function which changes the number of squares per side of the grid

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 let container = document.querySelector('#container');
+let sizeBtn = document.querySelector("button");
 let gridSize = 16;
-
 const containerWidth = 500;
-const squareSize = containerWidth / gridSize;
 
-for(let i = 0; i < gridSize * gridSize; i++){
-    const square = document.createElement("div");
-    square.classList.add("grid-square");
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "palevioletred"
-    });
+function createGrid() {
+    const squareSize = containerWidth / gridSize;
 
-    square.style.width = `${squareSize}px`;
-    square.style.height = `${squareSize}px`;
+    for(let i = 0; i < gridSize * gridSize; i++){
+        const square = document.createElement("div");
+        square.classList.add("grid-square");
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "palevioletred"
+        });
 
-    container.appendChild(square);
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        container.appendChild(square);
+    }
 }
+
+createGrid();
+
+function changeSquareNumber () {
+    let size = Number(prompt("Enter the Number of Squares per size. Note: 'Max 100'"));
+    if (size <= 100) {
+        gridSize = size;
+        container.innerHTML = '';
+        createGrid();
+    } else {
+        alert("Enter a number less or equal to 100");
+    }
+}
+
+sizeBtn.addEventListener("click", changeSquareNumber);
